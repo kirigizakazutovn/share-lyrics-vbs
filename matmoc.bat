ee0e8540715e34f279ff99f166611b074b23a028
Nguyễn Huy Hiếu <83147867+kirigizakazutovn@users.noreply.github.com>
2022-10-09 21:44:18 +0700

    Create matmoc.bat

---
+++ matmoc.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+msgbox"chang tim dau ra, mot nguoi nhu the"
+msgbox"moi cuoi xinh nhu hoa dang yeu, that tha"
+msgbox"cu em dem thuong nhau nhu qua bao nang mua"
+msgbox"mot doi nang niu, mot doi dua don"
+msgbox"khi binh minh om em ngu say trong long"
+msgbox"den khi gia nua van say dam em thoii"
